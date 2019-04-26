3ab2c2a14e54064a4f8fdc5bfe7166d218426edb
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2019-04-26 17:53:51 -0300

    first commit

---
+++ example/android/app/src/main/java/com/rafaelbarbosatec/tutorial_coach_mark_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rafaelbarbosatec.tutorial_coach_mark_demo;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
