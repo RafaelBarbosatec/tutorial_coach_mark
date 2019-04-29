c9b144e89ba7cd147ac249ea37aa9d876a661874
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2019-04-29 11:15:05 -0300

    adiciona gif

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,37 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TutorialCoachMark Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -376,7 +355,13 @@ class _MyHomePageState extends State<MyHomePage> {
     TutorialCoachMark(
       context,
       targets: targets,
-      colorShadow: Colors.red,
+      colorShadow: Colors.red, // DEFAULT Colors.black
+       // alignSkip: Alignment.bottomRight,
+       // textSkip: "SKIP",
+       // paddingFocus: 10,
+      finish: (){
+        print("finish");
+      },
       clickTarget: (target){
         print(target);
       },
